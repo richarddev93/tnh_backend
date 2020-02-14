0e798b04aa5df5ffa22ec9c344cc1a37a224aa14
richard <richardmachado.93@gmail.com>
2020-02-13 22:50:19 -0200

    Configuring the app

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['tnh-webservice.herokuapp.com ','localhost:8000','http://tnh-webservice.herokuapp.com/']
+ALLOWED_HOSTS = ['tnh-webservice.herokuapp.com ','localhost:8000','http://tnh-webservice.herokuapp.com/','191.243.82.129']
 
 
 # Application definition
